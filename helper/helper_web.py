dad9ee53a373282fdb945bc951888b65918b0872
Vinoth Palanivel <vinothp.nov29@gmail.com>
2025-04-08 22:37:21 +0530

    Made changes according to latest selenium version

--- helper/helper_web.py
+++ helper/helper_web.py
@@ -1,14 +1,18 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
  
 def get_browser(browser):
     if browser == "chrome":
-        options = Options()
-        options.headless = False
-        options.add_experimental_option("useAutomationExtension", False)
-        options.add_experimental_option("excludeSwitches",["enable-automation"])
-        driver = webdriver.Chrome(ChromeDriverManager('latest').install(), chrome_options=options)
+        # options = Options()
+        # options.headless = False
+        # options.add_experimental_option("useAutomationExtension", False)
+        # options.add_experimental_option("excludeSwitches",["enable-automation"]) 
+        # driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+        options = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=options)
+        
         driver.maximize_window()
         driver.get("https://opensource-demo.orangehrmlive.com/")
         return driver
